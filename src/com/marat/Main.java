a51132f08910c0ef5ea8d1744eda6d5d5237351e
eshmakar <59125020+eshmakar@users.noreply.github.com>
2020-01-22 14:06:41 +0300

    Update Main.java
    Добавил комментарии к коду

--- src/com/marat/Main.java
+++ src/com/marat/Main.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        long t1 = System.currentTimeMillis()+1000*60*5;
+        long t1 = System.currentTimeMillis()+1000*60*5; // здесь выбрано выполнение за 5 минут
         System.out.println("Выполняю...");
         FileWriter fw = new FileWriter("D:\\java\\test.txt", false);
-while (System.currentTimeMillis()<t1) {
-       fw.write("0");
+while (System.currentTimeMillis()<t1) { // если настоящее время меньше чем мы написали сверху от 5 минут....
+       fw.write("0"); // запишет в файл нолики
 }
         fw.close();
         System.out.println("Выполнил!");
